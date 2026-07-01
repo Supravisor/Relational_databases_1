c5c49a8e155a28627f585cabc82d3c0fa34017b5
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-07-01 14:05:58 +1200

    add cursor function

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ const connect = () => {
       document.editor.textbox.value+="\nconn = sqlite3.connect('" + databasePython.value + ".db')";
   }
 }
+
+const cursor = () => {
+  if (tablePython.value === "") {
+    return alert("Please enter a table name in the 'table' field, in the 'Read data from SQL database' section.");
+  } else {
+      document.editor.textbox.value+="\ncur = conn.cursor()\ncur.execute('SELECT * From " + tablePython.value + " LIMIT 5;')\nresults = cur.fetchall()\ndf = pd.DataFrame(results)\ncur.close()\nconn.close()";
+  }
+}
