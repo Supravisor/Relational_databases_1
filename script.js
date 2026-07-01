ac6a88682fd50a508926f086bfadd62399f58c09
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-07-01 13:56:17 +1200

    add connect function

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 
-// Read data using Python
+// Read data from SQL database
 let databasePython = document.getElementById("databasePython");
 let tablePython = document.getElementById("tablePython");
+
+const connect = () => {
+  if (databasePython.value === "") {
+    return alert("Please enter a database name in the 'database' field, in the 'Read data from SQL database' section.");
+  } else {
+      document.editor.textbox.value+="\nconn = sqlite3.connect('" + databasePython.value + ".db')";
+  }
+}
